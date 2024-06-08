e5b01ac4ad9579424917ef995ca678114cd27f26
Simeon Vibbi <svibbinyakeh@gmail.com>
2024-06-07 18:23:19 -0700

    setting

--- vue.config.js
+++ vue.config.js
@@ -2,14 +2,14 @@ const { defineConfig } = require('@vue/cli-service')
 
 module.exports = defineConfig({
   devServer: {
-    host: '0.0.0.0',
+    host: '0.0.0.0', 
     port: 8080,
     allowedHosts: 'all', 
     proxy: {
       '/api': {
-        target: 'https://api.github.com',
-        changeOrigin: true,
-        pathRewrite: { '^/api': '' }
+        target: 'https://api.github.com', 
+        changeOrigin: true, 
+        pathRewrite: { '^/api': '' } 
       }
     }
   }
